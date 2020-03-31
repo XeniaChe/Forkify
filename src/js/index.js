21b813ed30395ead0d238eb076cfa60e9a9ce5fc
Ksenia Troitskaja <xeniacserkun@gmail.com>
2020-03-31 22:11:00 +0200

    First class

--- src/js/index.js
+++ src/js/index.js
@@ -1,27 +1,7 @@
-// https://forkify-api.herokuapp.com/api/search
-/*
-import axios from 'axios';
+import Search from "./models/Search";
+const search = new Search('pizza');
 
-async function getResult(query) {
-    const res = await axios (`https://forkify-api.herokuapp.com/api/search?q=${query}`);
-    console.log(res);
-    
-};
+console.log(search);
+search.getResult();
 
-getResult('beetroot');
-*/
 
-async function getResult(query) {
-    try {
-        const res = await fetch(`https://forkify-api.herokuapp.com/api/search?q=${query}`);
-    const data = await res.json();
-    const recipies = data.recipes;
-    console.log(recipies);
-    console.log(recipies[0].recipe_id)
-    } catch (error) {
-        alert(error)
-    }
-    
-}
-
-getResult('beetroot')
--- src/js/models/Search.js
+++ src/js/models/Search.js
@@ -1,1 +1,35 @@
 // https://forkify-api.herokuapp.com/api/search
+/*
+import axios from 'axios';
+
+async function getResult(query) {
+    const res = await axios (`https://forkify-api.herokuapp.com/api/search?q=${query}`);
+    console.log(res);
+    
+};
+
+getResult('beetroot');
+*/
+
+export default  class Serach {
+    constructor(query) {
+        this.query = query;
+
+    }
+    async  getResult() {
+        try {
+         const res = await fetch(`https://forkify-api.herokuapp.com/api/search?q=${this.query}`);
+        const data = await res.json();
+        this.recipies = data.recipes;
+        console.log(this.recipies);
+        } catch (error) {
+            alert(error)
+        }
+        
+    }
+    
+    // getResult(); 
+}
+
+
+
